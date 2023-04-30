34ea4684c16d82ca14c00f8e04b4d5cc1a0af430
Yejay <demirkan.yejay@gmail.com>
2023-04-30 16:55:30 +0200

    first draft of the landing page

--- src/components/landingPageComponents/LandingPage.js
+++ src/components/landingPageComponents/LandingPage.js
@@ -1,73 +1,154 @@
-import React from 'react';
-import { Container, Box, Typography, TextField, Button, Avatar, Grid, Paper } from '@mui/material';
-import { useTheme } from '@emotion/react';
+import React, { useState } from 'react';
+import { Box, Typography, TextField, Avatar, Paper, IconButton, useTheme } from '@mui/material';
+import { PlayArrow, ArrowForward, ArrowBack, Filter1, Filter2, Filter3 } from '@mui/icons-material';
 
 function LandingPage() {
-    const theme = useTheme();
+	const theme = useTheme();
 
 	const mainContainerStyle = {
 		display: 'flex',
 		flexDirection: 'column',
 		alignItems: 'center',
 		justifyContent: 'center',
-		height: '100vh',
+		minHeight: 'calc(100vh - 96px)', // Updated
+		width: '100%',
+		background: `linear-gradient(${theme.palette.secondary.main}, white)`,
+		padding: '0',
 	};
 
 	const titleStyle = {
 		marginBottom: '32px',
+		fontSize: '5rem',
 	};
 
 	const inputBoxStyle = {
 		display: 'flex',
 		flexDirection: 'column',
 		alignItems: 'center',
-		padding: '24px',
-		marginBottom: '40px',
+		padding: '32px',
+		width: '25%',
+		backgroundColor: theme.palette.primary.main,
+		borderRadius: '4px',
+	};
+
+	const textFieldStyle = {
+		backgroundColor: theme.palette.secondary.light,
+		// borderRadius: '32px',
+		marginBottom: '16px',
 	};
 
 	const avatarGridStyle = {
+		display: 'flex',
+		alignItems: 'center',
 		marginTop: '16px',
 		marginBottom: '16px',
 	};
 
+	const playButtonStyle = {
+		width: '128px',
+		height: '48px',
+		backgroundColor: 'forestgreen',
+	};
+
 	const howToPlayStyle = {
-		position: 'absolute',
-		bottom: 0,
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
 		width: '100%',
+		minHeight: '48px',
 		padding: '16px',
-		backgroundColor: '#3f51b5',
-		color: 'white',
+		backgroundColor: 'transparent',
+		color: theme.palette.text.primary,
+		marginTop: '32px', // Added
+	};
+
+	const howToPlayListStyle = {
+		display: 'flex',
+		justifyContent: 'space-evenly',
+		alignItems: 'center',
+		width: '100%',
+		padding: '0',
+		listStyle: 'none',
+		fontSize: '3rem',
+	};
+
+	const listItemStyle = {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+	};
+
+	/**
+	 * state management for the color picker, same procedure can be done for the avatars
+	 * should be done with redux later, just for demonstration purposes
+	 */
+	const colors = ['Red', 'Green', 'Blue'];
+	const [currentColorIndex, setCurrentColorIndex] = useState(0);
+
+	const handlePreviousColor = () => {
+		setCurrentColorIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : colors.length - 1));
+	};
+
+	const handleNextColor = () => {
+		setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
 	};
 
 	const avatars = ['avatar1.png', 'avatar2.png', 'avatar3.png'];
+	const numberedIcons = [<Filter1 />, <Filter2 />, <Filter3 />];
 
 	return (
-		<Container style={mainContainerStyle}>
-			<Typography variant='h2' style={titleStyle}>
-				Triviosa
-			</Typography>
-			<Paper style={inputBoxStyle}>
-				<TextField label='Enter your name' variant='outlined' fullWidth />
-				<Grid container spacing={2} style={avatarGridStyle}>
-					{avatars.map((avatar) => (
-						<Grid item key={avatar}>
-							<Avatar alt={avatar} src={avatar} />
-						</Grid>
-					))}
-				</Grid>
-				<Button variant='contained' color='primary'>
-					Play
-				</Button>
-			</Paper>
-			<Box style={howToPlayStyle}>
-				<Typography variant='h6'>How to play</Typography>
-				<ul>
-					<li>Enter your name and choose an avatar</li>
-					<li>Click the "Play" button to start the game</li>
-					<li>Answer the quiz questions within the given time</li>
-				</ul>
+		<Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+			<Box style={mainContainerStyle}>
+				<Typography variant='h1' style={titleStyle}>
+					Triviosa
+				</Typography>
+				<Paper elevation={3} style={inputBoxStyle}>
+					<TextField label="What's your name?" variant='filled' fullWidth sx={textFieldStyle} />
+					<Typography variant='h6' style={{ marginTop: '16px', marginBottom: '8px' }}>
+						Choose an Avatar
+					</Typography>
+					<Box style={avatarGridStyle}>
+						<IconButton>
+							<ArrowBack />
+						</IconButton>
+						<Avatar alt='avatar' src={avatars[0]} sx={{ width: 80, height: 80 }} />
+						<IconButton>
+							<ArrowForward />
+						</IconButton>
+					</Box>
+					<Box style={avatarGridStyle}>
+						<IconButton onClick={handlePreviousColor}>
+							<ArrowBack />
+						</IconButton>
+						{/* Replace with the color preview component */}
+						<Typography variant='h6'>{colors[currentColorIndex]}</Typography>
+						<IconButton onClick={handleNextColor}>
+							<ArrowForward />
+						</IconButton>
+					</Box>
+					<IconButton color='secondary' style={playButtonStyle}>
+						<PlayArrow fontSize='large' />
+					</IconButton>
+				</Paper>
+				<Box style={howToPlayStyle}>
+					<Typography fontSize={'2rem'}>How to play</Typography>
+					<ul style={howToPlayListStyle}>
+						{[
+							'Choose 5 categories for the questions.',
+							'Choose a question from the board: 100 points are easier questions 1000 points are the hardest.',
+							'Answer questions before time runs out.',
+						].map((text, index) => (
+							<li key={index} style={listItemStyle}>
+								{numberedIcons[index]}
+								<Typography variant='body1'>{text}</Typography>
+							</li>
+						))}
+					</ul>
+				</Box>
 			</Box>
-		</Container>
+		</Box>
 	);
 }
 
