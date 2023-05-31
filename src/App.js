0c6848b971c46d308e090d54d8256ff12fe84687
Yejay <Sertan.ak1996@gmail.com>
2023-05-31 15:53:22 +0200

    delete Error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import LandingPage from './components/landingPageComponents/LandingPage';
 // import LobbyPageLayout from './components/lobbyComponents/LobbyPageLayout';
 // import PointSelectionPageLayout from "./components/pointSelectionComponents/PointSelectionPageLayout";
 // import { QuizPage } from "./components/quizPageComponent/QuizPage";
-// import ErrorPage from "./components/errorPageComponents/ErrorPage";
 
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
 			{/* <PointSelectionPageLayout /> */}
 			<LandingPage />
 			{/* <QuizPage/> */}
-			{/* <ErrorPage /> */}
 		</div>
 	);
 }
