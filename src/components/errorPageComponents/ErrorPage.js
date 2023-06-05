1cdd8b3621e14ab1894f3c0d426f96b82902ca53
Yejay <Sertan.ak1996@gmail.com>
2023-06-05 17:06:18 +0200

    error feature

--- src/components/errorPageComponents/ErrorPage.js
+++ src/components/errorPageComponents/ErrorPage.js
@@ -1,110 +1,237 @@
-import React from 'react';
-import { Box, useTheme, Paper, Typography, Grid, Button } from '@mui/material';
-import Header from '../landingPageComponents/modularComponents/Header';
-import JohnCenaImage from '../../asset/img/JohnCena.gif'
+// import React, { useEffect, useState } from 'react';
+// import { Box, useTheme, Paper, Typography, Grid, Button } from '@mui/material';
+// import Header from '../landingPageComponents/modularComponents/Header';
+// import JohnCenaImage from '../../asset/img/JohnCena.gif'
 
+
+// function ErrorPage() {
+//     const theme = useTheme();
+
+//     const mainContainerStyle = {
+//         display: 'flex',
+//         flexDirection: 'column',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         width: '100%',
+//         background: `linear-gradient(${theme.palette.secondary.main}, white)`,
+//     };
+//     const howToPlayStyle = {
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         width: '100%',
+//         padding: '16px',
+//         backgroundColor: 'transparent',
+//         color: theme.palette.primary,
+//     };
+
+//     const johnCenaStyle = {
+//         marginTop: 'auto',
+//         display: 'flex',
+//         justifyContent: 'center',
+//     };
+
+
+//     return (
+//         <Box
+//             sx={{
+//                 width: '100%',
+//                 display: 'flex',
+//                 flexDirection: 'column',
+//                 minHeight: '100vh',
+//             }}
+//         >
+//             <Box style={mainContainerStyle}>
+
+//                 <Header />
+//                 <Paper
+//                     elevation={3}
+//                     sx={{
+//                         display: "flex",
+//                         flexDirection: "column",
+//                         padding: theme.spacing(2),
+//                         width: "75%",
+//                         height: "75%",
+//                         backgroundColor: theme.palette.primary.main,
+//                         borderRadius: "4px",
+//                         alignItems: "center",
+//                         justifyContent: "center"
+//                     }}
+//                 >
+//                     <Box style={howToPlayStyle}>
+//                         <Grid container my={2} alignItems="center" justifyContent="center" >
+//                             <Typography variant='h3'>
+//                                 404 Error: Could not find page
+//                             </Typography>
+//                         </Grid>
+//                     </Box>
+//                     <Box sx={{
+//                         display: 'flex',
+//                         justifyContent: 'center',
+//                         alignItems: 'center',
+//                         bgcolor: theme.palette.secondary.light,
+//                         p: '16px',
+//                         width: '900px',
+//                         m: theme.spacing(2),
+//                         flexDirection: 'column',
+//                     }} >
+//                         <Box padding='20px'>
+//                             <Typography variant='h5' >
+//                                 But why couldn't this page be found?
+                              
+//                             </Typography>
+//                         </Box>
+//                         <Grid container my={4} rowSpacing={2} columnSpacing={1} m={4}>
+//                             <Grid item xs={6} ><Button
+//                                 bgcolor={theme.palette.primary.light}
+//                                 variant='contained' sx={{ width: '100%' }}>There is a Network Problem</Button></Grid>
+//                             <Grid item xs={6} ><Button
+//                                 bgcolor={theme.palette.primary.light}
+//                                 variant='contained' sx={{ width: '100%' }}>This page does not exist</Button></Grid>
+//                             <Grid item xs={6} ><Button
+//                                 bgcolor={theme.palette.primary.light}
+//                                 variant='contained' sx={{ width: '100%' }}>You can't see it</Button></Grid>
+//                             <Grid item xs={6} ><Button
+//                                 bgcolor={theme.palette.primary.light}
+//                                 variant='contained' sx={{ width: '100%' }}>It is a Layer 8 Problem</Button></Grid>
+//                         </Grid>
+//                     </Box>
+
+//                 </Paper>
+
+//                 {/* <Box 
+// 			{/* <Box style={johnCenaStyle}>
+//             <img src={JohnCenaImage} alt="John Cena" />
+//         </Box> */}
+//             </Box>
+//         </Box>
+//     );
+// }
+
+// export default ErrorPage;
+
+import React, { useEffect, useState } from 'react';
+import { Box, useTheme, Paper, Typography, Grid, Button } from '@mui/material';
 
 function ErrorPage() {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    const mainContainerStyle = {
+  const mainContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    background: `linear-gradient(${theme.palette.secondary.main}, white)`,
+  };
+  const howToPlayStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    padding: '16px',
+    backgroundColor: 'transparent',
+    color: theme.palette.primary,
+  };
+
+  const [text, setText] = useState('');
+  const [showButtons, setShowButtons] = useState(false);
+
+  useEffect(() => {
+    const message = "Test Text to test the second letter" //"But why couldn't this page be found?";
+    let currentIndex = 0;
+
+    const timer = setInterval(() => {
+      if (currentIndex === message.length) {
+        clearInterval(timer);
+        setShowButtons(true);
+      } else {
+        setText(prevText => prevText + message[currentIndex]);
+        currentIndex++;
+      }
+    }, 100);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        width: '100%',
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        background: `linear-gradient(${theme.palette.secondary.main}, white)`,
-    };
-    const howToPlayStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        padding: '16px',
-        backgroundColor: 'transparent',
-        color: theme.palette.primary,
-    };
-
-    const johnCenaStyle = {
-        marginTop: 'auto',
-        display: 'flex',
-        justifyContent: 'center',
-    };
-
-    return (
-        <Box
-            sx={{
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh',
-            }}
+        minHeight: '100vh',
+      }}
+    >
+      <Box style={mainContainerStyle}>
+        <Paper
+          elevation={3}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            padding: theme.spacing(2),
+            width: "75%",
+            height: "75%",
+            backgroundColor: theme.palette.primary.main,
+            borderRadius: "4px",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
         >
-            <Box style={mainContainerStyle}>
-
-                <Header />
-                <Paper
-                    elevation={3}
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        padding: theme.spacing(2),
-                        width: "75%",
-                        height: "75%",
-                        backgroundColor: theme.palette.primary.main,
-                        borderRadius: "4px",
-                        alignItems: "center",
-                        justifyContent: "center"
-                    }}
-                >
-                    <Box style={howToPlayStyle}>
-                        <Grid container my={2} alignItems="center" justifyContent="center" >
-                            <Typography variant='h3'>
-                                404 Error: Could not find page
-                            </Typography>
-                        </Grid>
-                    </Box>
-                    <Box sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        bgcolor: theme.palette.secondary.light,
-                        p: '16px',
-                        width: '900px',
-                        m: theme.spacing(2),
-                        flexDirection: 'column',
-                    }} >
-                        <Box padding='20px'>
-                            <Typography variant='h5' >
-                                But why couldn't this page be found?
-                            </Typography>
-                        </Box>
-                        <Grid container my={4} rowSpacing={2} columnSpacing={1} m={4}>
-                            <Grid item xs={6} ><Button
-                                bgcolor={theme.palette.primary.light}
-                                variant='contained' sx={{ width: '100%' }}>There is a Network Problem</Button></Grid>
-                            <Grid item xs={6} ><Button
-                                bgcolor={theme.palette.primary.light}
-                                variant='contained' sx={{ width: '100%' }}>This page does not exist</Button></Grid>
-                            <Grid item xs={6} ><Button
-                                bgcolor={theme.palette.primary.light}
-                                variant='contained' sx={{ width: '100%' }}>You can't see it</Button></Grid>
-                            <Grid item xs={6} ><Button
-                                bgcolor={theme.palette.primary.light}
-                                variant='contained' sx={{ width: '100%' }}>It is a Layer 8 Problem</Button></Grid>
-                        </Grid>
-                    </Box>
-
-                </Paper>
-
-                {/* <Box 
-			{/* <Box style={johnCenaStyle}>
-            <img src={JohnCenaImage} alt="John Cena" />
-        </Box> */}
+          <Box style={howToPlayStyle}>
+            <Grid container my={2} alignItems="center" justifyContent="center" >
+              <Typography variant='h3'>
+                404 Error: Could not find page
+              </Typography>
+            </Grid>
+          </Box>
+          <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            bgcolor: theme.palette.secondary.light,
+            p: '16px',
+            width: '900px',
+            m: theme.spacing(2),
+            flexDirection: 'column',
+          }}>
+            <Box padding='20px'>
+              <Typography variant='h5'>
+                {text.slice(0, text.length - 9)}
+              </Typography>
             </Box>
-        </Box>
-    );
+            {showButtons && (
+              <Grid container my={4} rowSpacing={2} columnSpacing={1} m={4}>
+                <Grid item xs={6}>
+                  <Button bgcolor={theme.palette.primary.light} variant="contained" sx={{ width: '100%' }}>
+                    There is a Network Problem
+                  </Button>
+                </Grid>
+                <Grid item xs={6}>
+                  <Button bgcolor={theme.palette.primary.light} variant="contained" sx={{ width: '100%' }}>
+                    This page does not exist
+                  </Button>
+                </Grid>
+                <Grid item xs={6}>
+                  <Button bgcolor={theme.palette.primary.light} variant="contained" sx={{ width: '100%' }}>
+                    You can't see it
+                  </Button>
+                </Grid>
+                <Grid item xs={6}>
+                  <Button bgcolor={theme.palette.primary.light} variant="contained" sx={{ width: '100%' }}>
+                    It is a Layer 8 Problem
+                  </Button>
+                </Grid>
+              </Grid>
+            )}
+          </Box>
+        </Paper>
+      </Box>
+    </Box>
+  );
 }
 
 export default ErrorPage;
+
