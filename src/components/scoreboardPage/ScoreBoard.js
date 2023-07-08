a13c7e7c4fd4034dcb7c057540bffb019ff1a76d
Yejay <demirkan.yejay@gmail.com>
2023-07-08 19:38:23 +0200

    update

--- src/components/scoreboardPage/ScoreBoard.js
+++ src/components/scoreboardPage/ScoreBoard.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, useTheme } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
 import { BaseColors } from '../../theme/theme';
+
 function ScoreBoard() {
 	const theme = useTheme();
 	const { leaderboard } = useSelector((state) => state.game);
 	const { players } = useSelector((state) => state.gameSettings);
-	// mapping of the socketId with the playername
+	const { avatars } = useSelector((state) => state.lobby);
 	const scoreBoard = leaderboard.map((element) => {
 		const playerName = players.find((player) => player.socketId === element.socketId)?.playerName;
 		return { ...element, playerName };
@@ -21,7 +24,7 @@ function ScoreBoard() {
 					<TableRow>
 						<TableCell>
 							<Typography variant='h6' color={BaseColors.mainWhite} pl={theme.spacing(4)}>
-								Player Name
+								Player
 							</Typography>
 						</TableCell>
 						<TableCell align='right'>
@@ -32,20 +35,26 @@ function ScoreBoard() {
 					</TableRow>
 				</TableHead>
 				<TableBody sx={{ bgcolor: theme.palette.secondary.light }}>
-					{sortedScoreBoard.map((row, index) => (
-						<TableRow key={index}>
-							<TableCell component='th' scope='row'>
-								<Typography variant='body' color='secondary' fontWeight={'bold'} pl={theme.spacing(4)}>
-									{row.playerName}
-								</Typography>
-							</TableCell>
-							<TableCell align='right'>
-								<Typography variant='body' color='secondary' fontWeight={'bold'} pr={theme.spacing(4)}>
-									{row.points}
-								</Typography>
-							</TableCell>
-						</TableRow>
-					))}
+					{sortedScoreBoard.map((row, index) => {
+						const player = players.find((player) => player.socketId === row.socketId);
+						return (
+							<TableRow key={index}>
+								<TableCell component='th' scope='row'>
+									<Box display='flex' alignItems='center' pl={theme.spacing(4)}>
+										<Avatar alt='avatar' src={avatars[player.avatarIndex]} sx={{ width: 24, height: 24, mr: theme.spacing(1) }} />
+										<Typography variant='body' color='secondary' fontWeight={'bold'}>
+											{row.playerName}
+										</Typography>
+									</Box>
+								</TableCell>
+								<TableCell align='right'>
+									<Typography variant='body' color='secondary' fontWeight={'bold'} pr={theme.spacing(4)}>
+										{row.points}
+									</Typography>
+								</TableCell>
+							</TableRow>
+						);
+					})}
 				</TableBody>
 			</Table>
 		</TableContainer>
