9a4cdeca0c1c3d66057f769bd37650f159f4b236
Yejay <demirkan.yejay@gmail.com>
2023-07-08 18:59:42 +0200

    added errorpage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/landingPageComponents/LandingPage';
 import LobbyPageLayout from './components/lobbyComponents/LobbyPageLayout';
 import PointSelectionPageLayout from './components/pointSelectionComponents/PointSelectionPageLayout';
+import ErrorPage from './components/errorPageComponents/ErrorPage';
 import { QuizPage } from './components/quizPageComponent/QuizPage';
 import { ScoreboardPage } from './components/scoreboardPage/ScoreboardPage';
 import socketManager from './services/SocketManager';
@@ -18,13 +19,14 @@ function App() {
 	}, []);
 
 	return (
-		<div className="App">
+		<div className='App'>
 			<Routes>
-				<Route path="/" element={<LandingPage />} />
-				<Route path="/lobby" element={<LobbyPageLayout />} />
-				<Route path="/pointSelection" element={<PointSelectionPageLayout />} />
-				<Route path="/quiz" element={<QuizPage />} />
-				<Route path="/scoreboard" element={<ScoreboardPage />} />
+				<Route path='/' element={<LandingPage />} />
+				<Route path='/lobby' element={<LobbyPageLayout />} />
+				<Route path='/pointSelection' element={<PointSelectionPageLayout />} />
+				<Route path='/quiz' element={<QuizPage />} />
+				<Route path='/scoreboard' element={<ScoreboardPage />} />
+				<Route path='*' element={<ErrorPage />} />
 			</Routes>
 		</div>
 	);
