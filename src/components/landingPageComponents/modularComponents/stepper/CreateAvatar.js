fbe5b8e7cc9f97b9c1d8b0d94e4db92a381d81f3
Yejay <demirkan.yejay@gmail.com>
2023-07-08 19:29:48 +0200

    cleanup

--- src/components/landingPageComponents/modularComponents/stepper/CreateAvatar.js
+++ src/components/landingPageComponents/modularComponents/stepper/CreateAvatar.js
@@ -3,12 +3,12 @@ import { Box, TextField, Avatar, Paper, IconButton, useTheme, FormControl, Butto
 import { setActiveStep, setIGN, setAvatarIndex } from '../../../../store/slices/landingPageSlices/lobbySlice';
 import { ArrowForward, ArrowBack } from '@mui/icons-material';
 import { useDispatch, useSelector } from 'react-redux';
-import socketManager from '../../../../services/SocketManager';
 
 function CreateAvatar() {
 	const theme = useTheme();
 	const { user } = useSelector((state) => state.auth);
-	const { avatars, avatarIndex, playerSocketID } = useSelector((state) => state.lobby);
+
+	const { avatars, avatarIndex } = useSelector((state) => state.lobby);
 	const { ign, activeStep } = useSelector((state) => state.lobby);
 
 	const dispatch = useDispatch();
@@ -18,16 +18,6 @@ function CreateAvatar() {
 		if (newIndex < 0) newIndex = avatars.length - 1;
 		if (newIndex === avatars.length) newIndex = 0;
 		dispatch(setAvatarIndex(newIndex));
-
-		// const dataToSend = {
-		// 	flag: 'SYNC_AVATAR',
-		// 	data: {
-		// 		socketId: playerSocketID,
-		// 		avatarIndex: newIndex,
-		// 	},
-		// };
-		// console.log('DATA TO SEND: ' + JSON.stringify(dataToSend));
-		// socketManager.lobbySynchro(dataToSend);
 	};
 
 	return (
@@ -42,9 +32,7 @@ function CreateAvatar() {
 				borderRadius: theme.spacing(4),
 				m: theme.spacing(3),
 				justifyContent: 'center',
-				alignContent: 'center',
 				alignItems: 'center',
-				justifyItems: 'center',
 			}}
 		>
 			<Box
@@ -60,54 +48,53 @@ function CreateAvatar() {
 					<TextField
 						label={user ? '' : "What's your name?"}
 						variant='filled'
-						sx={{ my: theme.spacing(4), bgcolor: theme.palette.primary.main }}
+						sx={{ my: theme.spacing(2), bgcolor: theme.palette.primary.main }}
 						value={ign}
 						placeholder={user ? user.user : ''}
 						onChange={(e) => dispatch(setIGN(e.target.value))}
 						color='secondary'
 					/>
-					<Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', my: theme.spacing(4), gap: theme.spacing(2) }}>
-						<IconButton onClick={() => handleAvatarChange('prev')}>
-							<ArrowBack />
-						</IconButton>
-						<Avatar alt='avatar' src={avatars[avatarIndex]} sx={{ width: 80, height: 80 }} />
-						<IconButton onClick={() => handleAvatarChange('next')}>
-							<ArrowForward />
-						</IconButton>
-					</Box>
+				</FormControl>
 
-					<Box>
-						<Box>
-							<Button
-								sx={{
-									m: theme.spacing(2),
-									py: theme.spacing(2),
-									borderRadius: theme.spacing(4),
-								}}
-								size='large'
-								variant='contained'
-								color='secondary'
-								onClick={() => {
-									if (!ign) {
-										toast.error({
-											position: 'bottom-right',
-											autoClose: 5000,
-											hideProgressBar: false,
-											closeOnClick: true,
-											pauseOnHover: true,
-											draggable: true,
-											progress: undefined,
-											theme: 'light',
-										});
-									} else {
-										dispatch(setActiveStep(activeStep + 1));
-									}
-								}}
-							>
-								Create Avatar
-							</Button>
-						</Box>
-					</Box>
+				<Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', my: theme.spacing(2), gap: theme.spacing(2) }}>
+					<IconButton onClick={() => handleAvatarChange('prev')}>
+						<ArrowBack />
+					</IconButton>
+					<Avatar alt='avatar' src={avatars[avatarIndex]} sx={{ width: 120, height: 120 }} />
+					<IconButton onClick={() => handleAvatarChange('next')}>
+						<ArrowForward />
+					</IconButton>
+				</Box>
+
+				<FormControl>
+					<Button
+						sx={{
+							m: theme.spacing(2),
+							py: theme.spacing(2),
+							borderRadius: theme.spacing(4),
+						}}
+						size='large'
+						variant='contained'
+						color='secondary'
+						onClick={() => {
+							if (!ign) {
+								toast.error({
+									position: 'bottom-right',
+									autoClose: 5000,
+									hideProgressBar: false,
+									closeOnClick: true,
+									pauseOnHover: true,
+									draggable: true,
+									progress: undefined,
+									theme: 'light',
+								});
+							} else {
+								dispatch(setActiveStep(activeStep + 1));
+							}
+						}}
+					>
+						Create Avatar
+					</Button>
 				</FormControl>
 			</Box>
 		</Paper>
